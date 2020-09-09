b1a98309a387a77dfb9a735c5350e41b6147f894
Monday Garfield <protocolmirror@gmail.com>
2020-09-09 18:23:51 +0900

    factory inital commit

---
+++ mirror_factory/src/contract.rs
@@ -0,0 +1,280 @@
+use cosmwasm_std::{
+    log, to_binary, Api, Binary, CosmosMsg, Decimal, Env, Extern, HandleResponse, HandleResult,
+    HumanAddr, InitResponse, Querier, StdError, StdResult, Storage, Uint128, WasmMsg,
+};
+
+use crate::msg::{
+    ConfigResponse, DistributionInfoResponse, HandleMsg, InitMsg, QueryMsg, StakingCw20HookMsg,
+    WhitelistInfoResponse,
+};
+use crate::state::{
+    read_config, read_distribution_info, read_whitelist_info, store_config,
+    store_distribution_info, store_whitelist_info, Config, DistributionInfo, WhitelistInfo,
+};
+
+use cw20::Cw20HandleMsg;
+
+pub fn init<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    msg: InitMsg,
+) -> StdResult<InitResponse> {
+    store_config(
+        &mut deps.storage,
+        &Config {
+            owner: deps.api.canonical_address(&env.message.sender)?,
+            staking_token: deps.api.canonical_address(&msg.staking_token)?,
+            mint_per_block: msg.mint_per_block,
+        },
+    )?;
+
+    Ok(InitResponse::default())
+}
+
+pub fn handle<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    msg: HandleMsg,
+) -> StdResult<HandleResponse> {
+    match msg {
+        HandleMsg::UpdateConfig {
+            owner,
+            mint_per_block,
+        } => try_update_config(deps, env, owner, mint_per_block),
+        HandleMsg::UpdateWeight { symbol, weight } => try_update_weight(deps, env, symbol, weight),
+        HandleMsg::Whitelist {
+            symbol,
+            weight,
+            token_contract,
+            mint_contract,
+            market_contract,
+            oracle_contract,
+            staking_contract,
+        } => try_whitelist(
+            deps,
+            env,
+            symbol,
+            weight,
+            token_contract,
+            mint_contract,
+            market_contract,
+            oracle_contract,
+            staking_contract,
+        ),
+        HandleMsg::Mint { symbol } => try_mint(deps, env, symbol),
+    }
+}
+
+pub fn try_update_config<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    owner: Option<HumanAddr>,
+    mint_per_block: Option<Uint128>,
+) -> HandleResult {
+    let mut config: Config = read_config(&deps.storage)?;
+    if config.owner != deps.api.canonical_address(&env.message.sender)? {
+        return Err(StdError::unauthorized());
+    }
+
+    if let Some(owner) = owner {
+        config.owner = deps.api.canonical_address(&owner)?;
+    }
+
+    if let Some(mint_per_block) = mint_per_block {
+        config.mint_per_block = mint_per_block;
+    }
+
+    store_config(&mut deps.storage, &config)?;
+
+    Ok(HandleResponse::default())
+}
+
+pub fn try_update_weight<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    symbol: String,
+    weight: Decimal,
+) -> HandleResult {
+    let config: Config = read_config(&deps.storage)?;
+    if config.owner != deps.api.canonical_address(&env.message.sender)? {
+        return Err(StdError::unauthorized());
+    }
+
+    let mut distribution_info = read_distribution_info(&deps.storage, symbol.to_string())?;
+    distribution_info.weight = weight;
+
+    store_distribution_info(&mut deps.storage, symbol.to_string(), &distribution_info)?;
+    Ok(HandleResponse {
+        messages: vec![],
+        log: vec![
+            log("action", "update_weight"),
+            log("symbol", &symbol),
+            log("weight", &weight.to_string()),
+        ],
+        data: None,
+    })
+}
+
+// only owner can exeucte whitelist
+#[allow(clippy::too_many_arguments)]
+pub fn try_whitelist<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    symbol: String,
+    weight: Decimal,
+    token_contract: HumanAddr,
+    mint_contract: HumanAddr,
+    market_contract: HumanAddr,
+    oracle_contract: HumanAddr,
+    staking_contract: HumanAddr,
+) -> HandleResult {
+    let config: Config = read_config(&deps.storage)?;
+    if config.owner != deps.api.canonical_address(&env.message.sender)? {
+        return Err(StdError::unauthorized());
+    }
+
+    if read_whitelist_info(&deps.storage, symbol.clone()).is_ok() {
+        return Err(StdError::generic_err(format!(
+            "whitelist {} already exists",
+            symbol
+        )));
+    }
+
+    store_whitelist_info(
+        &mut deps.storage,
+        symbol.clone(),
+        &WhitelistInfo {
+            token_contract: deps.api.canonical_address(&token_contract)?,
+            mint_contract: deps.api.canonical_address(&mint_contract)?,
+            market_contract: deps.api.canonical_address(&market_contract)?,
+            oracle_contract: deps.api.canonical_address(&oracle_contract)?,
+            staking_contract: deps.api.canonical_address(&staking_contract)?,
+        },
+    )?;
+
+    store_distribution_info(
+        &mut deps.storage,
+        symbol.clone(),
+        &DistributionInfo {
+            weight,
+            last_height: env.block.height,
+        },
+    )?;
+
+    Ok(HandleResponse {
+        messages: vec![],
+        log: vec![
+            log("action", "whitelist"),
+            log("symbol", &symbol),
+            log("weight", &weight.to_string()),
+        ],
+        data: None,
+    })
+}
+
+// Anyone can execute mint function to receive rewards
+pub fn try_mint<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    symbol: String,
+) -> HandleResult {
+    let config: Config = read_config(&deps.storage)?;
+    let whitelist_info: WhitelistInfo = read_whitelist_info(&deps.storage, symbol.to_string())?;
+    let distribution_info: DistributionInfo =
+        read_distribution_info(&deps.storage, symbol.to_string())?;
+
+    // mint_amount = weight * mint_per_block * (height - last_height)
+    let mint_amount = (config.mint_per_block * distribution_info.weight)
+        .multiply_ratio(env.block.height - distribution_info.last_height, 1u64);
+
+    store_distribution_info(
+        &mut deps.storage,
+        symbol.to_string(),
+        &DistributionInfo {
+            last_height: env.block.height,
+            ..distribution_info
+        },
+    )?;
+
+    // mint token to self and try send minted tokens to staking contract
+    Ok(HandleResponse {
+        messages: vec![
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: deps.api.human_address(&config.staking_token)?,
+                msg: to_binary(&Cw20HandleMsg::Mint {
+                    recipient: env.contract.address,
+                    amount: mint_amount,
+                })?,
+                send: vec![],
+            }),
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: deps.api.human_address(&config.staking_token)?,
+                msg: to_binary(&Cw20HandleMsg::Send {
+                    contract: deps.api.human_address(&whitelist_info.staking_contract)?,
+                    amount: mint_amount,
+                    msg: Some(to_binary(&StakingCw20HookMsg::DepositReward {})?),
+                })?,
+                send: vec![],
+            }),
+        ],
+        log: vec![
+            log("action", "mint"),
+            log("symbol", symbol),
+            log("mint_amount", mint_amount.to_string()),
+        ],
+        data: None,
+    })
+}
+
+pub fn query<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    msg: QueryMsg,
+) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::Config {} => to_binary(&query_config(deps)?),
+        QueryMsg::WhitelistInfo { symbol } => to_binary(&query_whitelist_info(deps, symbol)?),
+        QueryMsg::DistributionInfo { symbol } => to_binary(&query_distribution_info(deps, symbol)?),
+    }
+}
+
+pub fn query_config<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+) -> StdResult<ConfigResponse> {
+    let state = read_config(&deps.storage)?;
+    let resp = ConfigResponse {
+        owner: deps.api.human_address(&state.owner)?,
+        staking_token: deps.api.human_address(&state.staking_token)?,
+        mint_per_block: state.mint_per_block,
+    };
+
+    Ok(resp)
+}
+
+pub fn query_whitelist_info<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    symbol: String,
+) -> StdResult<WhitelistInfoResponse> {
+    let state = read_whitelist_info(&deps.storage, symbol)?;
+    let resp = WhitelistInfoResponse {
+        mint_contract: deps.api.human_address(&state.mint_contract)?,
+        market_contract: deps.api.human_address(&state.market_contract)?,
+        staking_contract: deps.api.human_address(&state.staking_contract)?,
+        token_contract: deps.api.human_address(&state.token_contract)?,
+        oracle_contract: deps.api.human_address(&state.oracle_contract)?,
+    };
+
+    Ok(resp)
+}
+
+pub fn query_distribution_info<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    symbol: String,
+) -> StdResult<DistributionInfoResponse> {
+    let state = read_distribution_info(&deps.storage, symbol)?;
+    let resp = DistributionInfoResponse {
+        last_height: state.last_height,
+        weight: state.weight,
+    };
+
+    Ok(resp)
+}
---
+++ mirror_factory/tests/integration.rs
@@ -0,0 +1,339 @@
+//! This integration test tries to run and call the generated wasm.
+//! It depends on a Wasm build being available, which you can create with `cargo wasm`.
+//! Then running `cargo integration-test` will validate we can properly call into that generated Wasm.
+//!
+//! You can easily convert unit tests to integration tests as follows:
+//! 1. Copy them over verbatim
+//! 2. Then change
+//!      let mut deps = mock_dependencies(20, &[]);
+//!    to
+//!      let mut deps = mock_instance(WASM, &[]);
+//! 3. If you access raw storage, where ever you see something like:
+//!      deps.storage.get(CONFIG_KEY).expect("no data stored");
+//!    replace it with:
+//!      deps.with_storage(|store| {
+//!          let data = store.get(CONFIG_KEY).expect("no data stored");
+//!          //...
+//!      });
+//! 4. Anywhere you see query(&deps, ...) you must replace it with query(&mut deps, ...)
+use cosmwasm_std::{
+    from_binary, log, to_binary, CosmosMsg, Decimal, Env, HandleResponse, HandleResult, HumanAddr,
+    InitResponse, StdError, Uint128, WasmMsg,
+};
+use cosmwasm_vm::testing::{handle, init, mock_env, mock_instance, query, MOCK_CONTRACT_ADDR};
+use cw20::Cw20HandleMsg;
+use mirror_factory::msg::{
+    ConfigResponse, DistributionInfoResponse, HandleMsg, InitMsg, QueryMsg, StakingCw20HookMsg,
+    WhitelistInfoResponse,
+};
+
+fn mock_env_height(signer: &HumanAddr, height: u64, time: u64) -> Env {
+    let mut env = mock_env(signer, &[]);
+    env.block.height = height;
+    env.block.time = time;
+    env
+}
+
+// This line will test the output of cargo wasm
+static WASM: &[u8] = include_bytes!("../target/wasm32-unknown-unknown/release/mirror_factory.wasm");
+// You can uncomment this line instead to test productionified build from rust-optimizer
+// static WASM: &[u8] = include_bytes!("../contract.wasm");
+
+#[test]
+fn proper_initialization() {
+    let mut deps = mock_instance(WASM, &[]);
+
+    let msg = InitMsg {
+        staking_token: HumanAddr("token0000".to_string()),
+        mint_per_block: Uint128(100u128),
+    };
+
+    let env = mock_env("addr0000", &[]);
+    let _res: InitResponse = init(&mut deps, env, msg).unwrap();
+
+    // it worked, let's query the state
+    let res = query(&mut deps, QueryMsg::Config {}).unwrap();
+    let config: ConfigResponse = from_binary(&res).unwrap();
+    assert_eq!("addr0000", config.owner.as_str());
+    assert_eq!("token0000", config.staking_token.as_str());
+    assert_eq!(Uint128(100u128), config.mint_per_block);
+}
+
+#[test]
+fn test_update_config() {
+    let mut deps = mock_instance(WASM, &[]);
+
+    let msg = InitMsg {
+        staking_token: HumanAddr("token0000".to_string()),
+        mint_per_block: Uint128(100u128),
+    };
+
+    let env = mock_env("addr0000", &[]);
+    let _res: InitResponse = init(&mut deps, env, msg).unwrap();
+
+    // upate owner
+    let msg = HandleMsg::UpdateConfig {
+        owner: Some(HumanAddr::from("addr0001")),
+        mint_per_block: None,
+    };
+
+    let env = mock_env("addr0000", &[]);
+    let _res: HandleResponse = handle(&mut deps, env, msg).unwrap();
+    let res = query(&mut deps, QueryMsg::Config {}).unwrap();
+    let config: ConfigResponse = from_binary(&res).unwrap();
+    assert_eq!("addr0001", config.owner.as_str());
+    assert_eq!("token0000", config.staking_token.as_str());
+    assert_eq!(Uint128(100u128), config.mint_per_block);
+
+    // update rest part
+    let msg = HandleMsg::UpdateConfig {
+        owner: None,
+        mint_per_block: Some(Uint128(200u128)),
+    };
+
+    let env = mock_env("addr0001", &[]);
+    let _res: HandleResponse = handle(&mut deps, env, msg).unwrap();
+    let res = query(&mut deps, QueryMsg::Config {}).unwrap();
+    let config: ConfigResponse = from_binary(&res).unwrap();
+    assert_eq!("addr0001", config.owner.as_str());
+    assert_eq!("token0000", config.staking_token.as_str());
+    assert_eq!(Uint128(200u128), config.mint_per_block);
+
+    // failed unauthoirzed
+    let msg = HandleMsg::UpdateConfig {
+        owner: None,
+        mint_per_block: Some(Uint128(200u128)),
+    };
+
+    let env = mock_env("addr0000", &[]);
+    let res: HandleResult = handle(&mut deps, env, msg);
+    match res.unwrap_err() {
+        StdError::Unauthorized { .. } => {}
+        _ => panic!("Must return unauthorized error"),
+    }
+}
+
+#[test]
+fn test_whitelist() {
+    let mut deps = mock_instance(WASM, &[]);
+
+    let msg = InitMsg {
+        staking_token: HumanAddr("token0000".to_string()),
+        mint_per_block: Uint128(100u128),
+    };
+
+    let env = mock_env("addr0000", &[]);
+    let _res: InitResponse = init(&mut deps, env, msg).unwrap();
+
+    let msg = HandleMsg::Whitelist {
+        symbol: "mAPPL".to_string(),
+        weight: Decimal::from_ratio(15u64, 10u64),
+        mint_contract: HumanAddr::from("mint0000"),
+        market_contract: HumanAddr::from("market0000"),
+        oracle_contract: HumanAddr::from("oracle0000"),
+        token_contract: HumanAddr::from("token0000"),
+        staking_contract: HumanAddr::from("staking0000"),
+    };
+    let env = mock_env("addr0000", &[]);
+    let res: HandleResponse = handle(&mut deps, env.clone(), msg.clone()).unwrap();
+
+    assert_eq!(
+        res.log,
+        vec![
+            log("action", "whitelist"),
+            log("symbol", "mAPPL"),
+            log("weight", "1.5")
+        ]
+    );
+
+    let res = query(
+        &mut deps,
+        QueryMsg::WhitelistInfo {
+            symbol: "mAPPL".to_string(),
+        },
+    )
+    .unwrap();
+    let res: WhitelistInfoResponse = from_binary(&res).unwrap();
+    assert_eq!(res.mint_contract, HumanAddr::from("mint0000"));
+    assert_eq!(res.market_contract, HumanAddr::from("market0000"));
+    assert_eq!(res.oracle_contract, HumanAddr::from("oracle0000"));
+    assert_eq!(res.token_contract, HumanAddr::from("token0000"));
+    assert_eq!(res.staking_contract, HumanAddr::from("staking0000"));
+
+    let res = query(
+        &mut deps,
+        QueryMsg::DistributionInfo {
+            symbol: "mAPPL".to_string(),
+        },
+    )
+    .unwrap();
+    let res: DistributionInfoResponse = from_binary(&res).unwrap();
+    assert_eq!(res.weight, Decimal::from_ratio(15u64, 10u64));
+    assert_eq!(res.last_height, 12345u64);
+
+    let res: HandleResult = handle(&mut deps, env.clone(), msg.clone());
+    match res {
+        Err(StdError::GenericErr { msg, .. }) => assert_eq!(msg, "whitelist mAPPL already exists"),
+        _ => panic!("DO NOT ENTER HERE"),
+    }
+
+    let env = mock_env("addr0001", &[]);
+    let res: HandleResult = handle(&mut deps, env, msg);
+    match res {
+        Err(StdError::Unauthorized { .. }) => {}
+        _ => panic!("DO NOT ENTER HERE"),
+    }
+}
+
+#[test]
+fn test_update_weight() {
+    let mut deps = mock_instance(WASM, &[]);
+
+    let msg = InitMsg {
+        staking_token: HumanAddr("token0000".to_string()),
+        mint_per_block: Uint128(100u128),
+    };
+
+    let env = mock_env("addr0000", &[]);
+    let _res: InitResponse = init(&mut deps, env, msg).unwrap();
+
+    let msg = HandleMsg::Whitelist {
+        symbol: "mAPPL".to_string(),
+        weight: Decimal::from_ratio(15u64, 10u64),
+        mint_contract: HumanAddr::from("mint0000"),
+        market_contract: HumanAddr::from("market0000"),
+        oracle_contract: HumanAddr::from("oracle0000"),
+        token_contract: HumanAddr::from("token0000"),
+        staking_contract: HumanAddr::from("staking0000"),
+    };
+    let env = mock_env("addr0000", &[]);
+    let _res: HandleResponse = handle(&mut deps, env.clone(), msg).unwrap();
+
+    let msg = HandleMsg::UpdateWeight {
+        symbol: "mAPPL".to_string(),
+        weight: Decimal::from_ratio(2u64, 1u64),
+    };
+    let res: HandleResponse = handle(&mut deps, env.clone(), msg).unwrap();
+    assert_eq!(
+        res.log,
+        vec![
+            log("action", "update_weight"),
+            log("symbol", "mAPPL"),
+            log("weight", "2")
+        ]
+    );
+
+    let res = query(
+        &mut deps,
+        QueryMsg::DistributionInfo {
+            symbol: "mAPPL".to_string(),
+        },
+    )
+    .unwrap();
+    let res: DistributionInfoResponse = from_binary(&res).unwrap();
+    assert_eq!(res.weight, Decimal::from_ratio(2u64, 1u64));
+}
+
+#[test]
+fn test_mint() {
+    let mut deps = mock_instance(WASM, &[]);
+
+    let msg = InitMsg {
+        staking_token: HumanAddr("token0000".to_string()),
+        mint_per_block: Uint128(100u128),
+    };
+
+    let env = mock_env("addr0000", &[]);
+    let _res: InitResponse = init(&mut deps, env, msg).unwrap();
+
+    let msg = HandleMsg::Whitelist {
+        symbol: "mAPPL".to_string(),
+        weight: Decimal::from_ratio(15u64, 10u64),
+        mint_contract: HumanAddr::from("mint0000"),
+        market_contract: HumanAddr::from("market0000"),
+        oracle_contract: HumanAddr::from("oracle0000"),
+        token_contract: HumanAddr::from("token0000"),
+        staking_contract: HumanAddr::from("staking0000"),
+    };
+    let env = mock_env("addr0000", &[]);
+    let _res: HandleResponse = handle(&mut deps, env.clone(), msg).unwrap();
+
+    // height is not increased so zero amount will be minted
+    let msg = HandleMsg::Mint {
+        symbol: "mAPPL".to_string(),
+    };
+    let res: HandleResponse = handle(&mut deps, env, msg).unwrap();
+    assert_eq!(
+        res.log,
+        vec![
+            log("action", "mint"),
+            log("symbol", "mAPPL"),
+            log("mint_amount", "0"),
+        ]
+    );
+
+    assert_eq!(
+        res.messages,
+        vec![
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: HumanAddr::from("token0000"),
+                msg: to_binary(&Cw20HandleMsg::Mint {
+                    recipient: HumanAddr::from(MOCK_CONTRACT_ADDR),
+                    amount: Uint128::zero(),
+                })
+                .unwrap(),
+                send: vec![],
+            }),
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: HumanAddr::from("token0000"),
+                msg: to_binary(&Cw20HandleMsg::Send {
+                    contract: HumanAddr::from("staking0000"),
+                    amount: Uint128::zero(),
+                    msg: Some(to_binary(&StakingCw20HookMsg::DepositReward {}).unwrap()),
+                })
+                .unwrap(),
+                send: vec![],
+            }),
+        ],
+    );
+
+    // one height increase
+    let msg = HandleMsg::Mint {
+        symbol: "mAPPL".to_string(),
+    };
+    let env = mock_env_height(&HumanAddr::from("addr0000"), 12346u64, 12345u64);
+    let res: HandleResponse = handle(&mut deps, env, msg).unwrap();
+    assert_eq!(
+        res.log,
+        vec![
+            log("action", "mint"),
+            log("symbol", "mAPPL"),
+            log("mint_amount", "150"),
+        ]
+    );
+
+    assert_eq!(
+        res.messages,
+        vec![
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: HumanAddr::from("token0000"),
+                msg: to_binary(&Cw20HandleMsg::Mint {
+                    recipient: HumanAddr::from(MOCK_CONTRACT_ADDR),
+                    amount: Uint128(150u128),
+                })
+                .unwrap(),
+                send: vec![],
+            }),
+            CosmosMsg::Wasm(WasmMsg::Execute {
+                contract_addr: HumanAddr::from("token0000"),
+                msg: to_binary(&Cw20HandleMsg::Send {
+                    contract: HumanAddr::from("staking0000"),
+                    amount: Uint128(150u128),
+                    msg: Some(to_binary(&StakingCw20HookMsg::DepositReward {}).unwrap()),
+                })
+                .unwrap(),
+                send: vec![],
+            }),
+        ],
+    );
+}
