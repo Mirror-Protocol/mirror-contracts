a6c613aacbb67d2641080d689b815bf3df36b1e3
Yun Yeo <sw.yunsuk@gmail.com>
2020-09-09 20:29:48 +0900

    change entry_points function of token contract to entry_points withtout migration

--- mirror_token/src/lib.rs
+++ mirror_token/src/lib.rs
@@ -5,4 +5,4 @@ pub mod msg;
 pub mod state;
 
 #[cfg(all(target_arch = "wasm32", not(feature = "library")))]
-cosmwasm_std::create_entry_points_with_migration!(contract);
+cosmwasm_std::create_entry_points!(contract);
