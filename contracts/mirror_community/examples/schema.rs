d8365b5b530880d5aa30986b3f1abba217e9a615
Yun Yeo <sw.yunsuk@gmail.com>
2020-11-17 16:07:03 +0900

    update binary & schema

--- contracts/mirror_community/examples/schema.rs
+++ contracts/mirror_community/examples/schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use mirror_collector::msg::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
+use mirror_community::msg::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
