ce6a473798715b4ca94016287ad58adb1531fa3a
csanti <carlos.san.rol@gmail.com>
2021-08-10 17:43:32 +0900

    bump collateral_oracle contract

--- contracts/mirror_collateral_oracle/src/math.rs
+++ contracts/mirror_collateral_oracle/src/math.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{Decimal, Uint128};
 
-const DECIMAL_FRACTIONAL: Uint128 = Uint128(1_000_000_000u128);
+const DECIMAL_FRACTIONAL: Uint128 = Uint128::new(1_000_000_000u128);
 
 /// return a / b
 pub fn decimal_division(a: Decimal, b: Decimal) -> Decimal {
